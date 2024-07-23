caf4770cffb85038a3aa67d260163ba57f1bb722
Abdul Basit <127122706+Adrein10@users.noreply.github.com>
2024-07-23 16:55:28 +0500

    Adrein

---
+++ adrein/index.js
@@ -0,0 +1,8 @@
+var usernam0 = "abc";
+var a = 2;
+var b = 6;
+var c = a + b;
+console.log(username1);
+console.log(typeof username1);
+console.log(c);
+console.log(typeof c);
---
+++ adrein/index.ts
@@ -0,0 +1,8 @@
+let usernam0: string = "abc";
+let a : number = 2;
+let b : number = 6;
+let c = a + b;
+console.log(username1);
+console.log(typeof username1);
+console.log(c);
+console.log(typeof c);
